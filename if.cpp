0f20fd16328e1d39640504f0fbe045985251a645
bueno <bueno.mateus@aluno.ifsp.edu.br>
2024-09-05 10:31:03 -0300

    4

--- if.cpp
+++ if.cpp
@@ -5,12 +5,13 @@
 int main() {
     setlocale(LC_ALL, "Portuguese");
     int num;
-    float divisao;
+    float metade;
     
     printf("Digite um número: ");
     scanf("%d", &num);
     
     if (num > 20) 
-        divisao = num / 2.0f; 
-        printf("O número é maior que 20. A divisão por 2 é: %.2f \n", divisao);
+        metade = num / 2.0f; 
+        printf("O número é maior que 20. A divisão por 2 é: %.2f \n", metade);
 }
+
