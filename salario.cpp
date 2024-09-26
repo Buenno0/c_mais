8bb4cc9ee8818a516954ecbd75c10517eb1f3755
bueno <bueno.mateus@aluno.ifsp.edu.br>
2024-09-26 10:42:24 -0300

    Refactor salary calculation and update input/output formatting

--- salario.cpp
+++ salario.cpp
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
-int main(){
-	setlocale(LC_ALL, "Portuguese");
 
-    int salario;
-    int aumento;
-    int novoSalario;
+int main() {
+    setlocale(LC_ALL, "Portuguese");
+
+    float salario;
+    float aumento;
+    float novoSalario;
 
     printf("Digite o salário atual: ");
-    scanf("%d", &salario);
+    scanf("%f", &salario);
 
-    if (salario < 1500){
+    if (salario < 1500) {
         aumento = salario * 0.15;
         novoSalario = salario + aumento;
-        printf("O novo salário é: %d \n", novoSalario);
-    } else if (salario >= 1500 && salario <= 3000){
+        printf("O novo salário é: R$%.2f\n", novoSalario);
+    } else if (salario >= 1500 && salario <= 3000) {
         aumento = salario * 0.10;
         novoSalario = salario + aumento;
-        printf("O novo salário é: %d \n", novoSalario);
+        printf("O novo salário é: R$%.2f\n", novoSalario);
     } else {
         aumento = salario * 0.05;
         novoSalario = salario + aumento;
-        printf("O novo salário é: %d \n", novoSalario);
+        printf("O novo salário é: R$%.2f\n", novoSalario);
     }
-
 }
